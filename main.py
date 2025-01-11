60f5da07c1d644f371b153c2622cea061170fd78
TalyShasho <tshasho1b@gmail.com>
2025-01-11 16:03:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from Scr.average import get_average_valence_grid
 from Scr.dif_resp_to_music_type import plot_valence_histogram
 from Scr.correlation_musictyoe_emotionalresp import calculate_pearson_correlation
 from Scr.correlation_musictyoe_emotionalresp import correlations_to_dataframe
+from tools.data_filter import filter_to_new_excel  # Import the function
+
+# Define folder path and selected columns
+folder_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data\trial_data"
+selected_columns = ['session', 'music_type', 'valence_rating', 'RT']  # Specify the columns you need
+final_folder_path = r"C:\Users\Home\Desktop\Studies\Phyton\projects 2024-2025\Project_2\data"
+
+# Call the function from functions.py
+filter_to_new_excel(folder_path, selected_columns, final_folder_path)
 
 file_path = '/mnt/data/compiled_data.xlsx'  
 average_valence = get_average_valence_grid(file_path)
